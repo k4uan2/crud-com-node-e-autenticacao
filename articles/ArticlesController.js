9231989876d4437030aac78520079f503bfd0437
Kauan Victor <k4uan222@gmail.com>
2023-07-05 15:49:56 -0300

    Adicionando TinyMCE na aplicação e finalizando formulário para criar artigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category")
 
 router.get("/articles", (req, res) => {
   res.send("<h1> Artigos </h1>")
 })
 
 router.get("/admin/article/new", (req, res) => {
-  res.send("<h1>Criar novo artigo:</h1>")
+  Category.findAll().then(categories => {
+    res.render("admin/articles/new", {categories: categories})
+  })
 })
 
 module.exports = router;
