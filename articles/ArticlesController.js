4943350fc3ddc80b0b13210e7043ecb768d35fab
Kauan Victor <k4uan222@gmail.com>
2023-07-06 14:56:46 -0300

    editando artigos existentes

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -49,5 +49,36 @@ router.post("/articles/delete", (req, res) => {
   }
 })
 
+router.get("/admin/articles/edit/:id", (req, res) => {
+  var id = req.params.id
+  Article.findByPk(id).then(article => {
+    if(article != undefined){
+      Category.findAll().then((categories) => {
+        res.render("admin/articles/edit", ({article: article, categories: categories}))
+      })
+    }else{
+      res.redirect("/")
+    }
+  }).catch(err => {
+    res.redirect("/")
+  })
+})
+
+router.post("/articles/update", (req, res) => {
+  var id = req.body.id
+  var title = req.body.title
+  var category = req.body.category
+  var body = req.body.body
+
+  Article.update({title: title, category: category, body: body, slug: slugify(title)}, {
+    where: {
+      id: id
+    }
+  }).then(()=> {
+    res.redirect("/admin/articles")
+  }).catch(err => {
+    res.redirect("/")
+  })
+})
 
 module.exports = router;
